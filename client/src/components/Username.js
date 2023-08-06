65b2b6b1bdff4a8f97cac59164b6917b25e1e76a
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-08-06 13:29:25 +0200

    End of login ui

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import avatar from '../assets/profile.png'
+import styles from "../styles/Username.module.css"
 const Username = () => {
   return (
     <div className='container mx-auto'>
-      <div className='flex justify-center h-screen'>
-        <div>
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass}>
             <div className='title flex flex-col items-center'>
                 <h4 className='text-5xl font-bold'>
                    Hello Again
@@ -15,14 +17,14 @@ const Username = () => {
             </div>
             <form className="py-1">
                 <div className='profile flex justify-center py-4'>
-                    <img src='' alt='avatar'/>
+                    <img src={avatar} alt='avatar' className={styles.profile_img}/>
                 </div>
-                <div className='textbox'>
-                    <input type='text' placeholder='Username'/>
-                    <button type='submit' >Let's go</button>
+                <div className='textbox flex flex-col items-center gap-6'>
+                    <input className={styles.textbox} type='text' placeholder='Username'/>
+                    <button className={styles.btn} type='submit' >Let's go</button>
                 </div>
                 <div className='text-center py-4'>
-                    <span className='text-gray-500 '>Not a member <Link className='text-red-500' href='/register'>Register</Link></span>
+                    <span className='text-gray-500 '>Not a member <Link className='text-red-500' to='/register'>Register</Link></span>
                 </div>
             </form>
         </div>
