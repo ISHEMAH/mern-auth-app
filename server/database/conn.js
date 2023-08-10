2b902048020577a69232b80f90a5a39afab4c4e1
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-08-10 16:57:35 +0200

    database connection

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
-async function connect(){
-    const mongod = await MongoMemoryServer.create()
-    const getUrl = mongod.getUrl()
+async function connect() {
+    try {
+        const mongod = await MongoMemoryServer.create();
+        const getUrl = mongod.getUri();
 
-    const db = await mongoose.connect(getUrl)
-    console.log("Database connected")
-    return db
+        const db = await mongoose.connect(getUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected");
+        return db;
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        throw error;
+    }
 }
 
 export default connect;
