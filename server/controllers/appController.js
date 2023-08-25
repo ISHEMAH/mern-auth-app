ff4449059a9f0d10cb3abdd16dc4c520cb64b2a2
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-08-26 01:05:49 +0200

    register and login

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,7 @@
 import UserModel from "../model/User.model.js";
 import bcrypt from "bcrypt";
+import  jwt  from "jsonwebtoken";
+import ENV from '../config.js'
 
 export async function register(req, res) {
     try {
@@ -37,7 +39,37 @@ export async function register(req, res) {
 }
 
 export async function login(req, res) {
-    // Implement your login logic here
+    const {username, password} = req.body;
+    try{
+        UserModel.findOne({username})
+        .then(user => {
+            bcrypt.compare(password,user.password)
+            .then(passwordCheck => {
+                if(!passwordCheck) return res.status(400).send({error:"Don't have Password"})
+
+                // create jwt token
+                const token = jwt.sign({
+                    userId: user._id,
+                    username:user.username
+                    
+                },ENV.JWT_SECRET,{expiresIn:"24h"})
+
+                return res.status(200).send({
+                    msg:"Login Successful...",
+                    username: user.username,
+                    token
+
+                })
+            })
+            .catch(error => {
+                return res.status(400).send({error:"Password does not match"})
+            })
+        })
+        .catch(error => {
+            return res.status(404).send({error: "User not found"})
+        })
+    }catch(error){
+        return res.status(500).send({error})    }
     res.json({ msg: "Login route" });
 }
 
