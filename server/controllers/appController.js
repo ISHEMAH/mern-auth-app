898aeff72c5fd2417e876081ce56b3626c16f3e3
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-08-30 10:33:58 +0200

    register and login on server

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -38,39 +38,43 @@ export async function register(req, res) {
     }
 }
 
-export async function login(req, res) {
-    const {username, password} = req.body;
-    try{
-        UserModel.findOne({username})
-        .then(user => {
-            bcrypt.compare(password,user.password)
-            .then(passwordCheck => {
-                if(!passwordCheck) return res.status(400).send({error:"Don't have Password"})
+export async function login(req,res){
+   
+    const { username, password } = req.body;
 
-                // create jwt token
-                const token = jwt.sign({
-                    userId: user._id,
-                    username:user.username
-                    
-                },ENV.JWT_SECRET,{expiresIn:"24h"})
+    try {
+        
+        UserModel.findOne({ username })
+            .then(user => {
+                bcrypt.compare(password, user.password)
+                    .then(passwordCheck => {
 
-                return res.status(200).send({
-                    msg:"Login Successful...",
-                    username: user.username,
-                    token
+                        if(!passwordCheck) return res.status(400).send({ error: "Don't have Password"});
 
-                })
+                        // create jwt token
+                        const token = jwt.sign({
+                                        userId: user._id,
+                                        username : user.username
+                                    }, ENV.JWT_SECRET , { expiresIn : "24h"});
+
+                        return res.status(200).send({
+                            msg: "Login Successful...!",
+                            username: user.username,
+                            token
+                        });                                    
+
+                    })
+                    .catch(error =>{
+                        return res.status(400).send({ error: "Password does not Match"})
+                    })
             })
-            .catch(error => {
-                return res.status(400).send({error:"Password does not match"})
+            .catch( error => {
+                return res.status(404).send({ error : "Username not Found"});
             })
-        })
-        .catch(error => {
-            return res.status(404).send({error: "User not found"})
-        })
-    }catch(error){
-        return res.status(500).send({error})    }
-    res.json({ msg: "Login route" });
+
+    } catch (error) {
+        return res.status(500).send({ error});
+    }
 }
 
 export async function getUser(req, res) {
