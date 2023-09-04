481883c271df0fa21d926202fd2d1aeda4b22289
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-09-04 11:44:51 +0200

    getuser initialised

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -3,6 +3,22 @@ import bcrypt from "bcrypt";
 import  jwt  from "jsonwebtoken";
 import ENV from '../config.js'
 
+// middleware for verify user
+
+export async function verifyUser(req,res,next){
+    try{
+        const {username} = req.method == "GET" ? req.query : req.body
+
+        // check the user existance
+        let exist = await UserModel.findOne({username})
+        if(!exist) return res.status(404).send({error:"Can't find user!"})
+        next()
+    }
+    catch(error){
+        return res.status(404).send({error: "Authenticate error"})
+    }
+}
+
 export async function register(req, res) {
     try {
         const { username, password, profile, email } = req.body;
@@ -77,9 +93,29 @@ export async function login(req,res){
     }
 }
 
-export async function getUser(req, res) {
-    // Implement your logic to get user details here
-    res.json({ msg: "Get user route" });
+export async function getUser(req,res){
+    
+    const { username } = req.params;
+
+    try {
+        
+        if(!username) return res.status(501).send({ error: "Invalid Username"});
+
+        UserModel.findOne({ username }, function(err, user){
+            if(err) return res.status(500).send({ err });
+            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+
+            /** remove password from user */
+            // mongoose return unnecessary data with object so convert it into json
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+
+            return res.status(201).send(rest);
+        })
+
+    } catch (error) {
+        return res.status(404).send({ error : "Cannot Find User Data"});
+    }
+
 }
 
 export async function updateUser(req, res) {
--- server/router/router.js
+++ server/router/router.js
@@ -2,19 +2,18 @@ import { Router } from "express";
 const router = Router()
 import * as controller from '../controllers/appController.js';
 
-
 // post methods
 router.route('/register').post(controller.register); 
 router.route('/registerMail').post()
 router.route('/authenticate').post()
-router.route('/login').post(controller.login);
+router.route('/login').post(controller.verifyUser,controller.login);
 
 
 // get methods
-router.route('/user/:useername').get()
-router.route('/generateOTP').get()
-router.route('/verifyOTP').get()
-router.route('/createResetSession').get()
+router.route('/user/:username').get(controller.getUser)
+router.route('/generateOTP').get(controller.generateOTP)
+router.route('/verifyOTP').get(controller.verifyOTP)
+router.route('/createResetSession').get(controller.createResetSession)
 
 
 // put methods
